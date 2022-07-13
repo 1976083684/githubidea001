845d82ab2ae70bd6bec506c7cc09c37fefb15a15
mjh <1976083684@qq.com>
2022-07-13 15:18:57 +0800

    =提交

---
+++ src/com/qst/HelloWord.java
@@ -0,0 +1,15 @@
+package com.qst;
+
+/**
+ * @author:CoolTiger
+ * @title: HelloWord
+ * @projectName github_test
+ * @description: TODO
+ * @date:2022/07/13/15:09:12
+ **/
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("helloword");
+        System.out.println("df");
+    }
+}
